209158db87e48b059a6167b6ad511b76771da203
Danciu <alexandru.danciu@man.eu>
2019-02-28 17:12:28 +0100

    Added new output message

--- rpi-tensorflow/checkDependencies.py
+++ rpi-tensorflow/checkDependencies.py
@@ -1,3 +1,5 @@
+print("Checking dependencies")
+
 modules = set(["numpy", "os", "six.moves.urllib", "sys", "tarfile", "tensorflow", "zipfile", "cv2", "collections", "io", "matplotlib", "PIL", "utils"])
 
 for module in modules:
