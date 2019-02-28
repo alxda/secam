f0a7fce05e1ec255a75cead31d0f3e48d6d95bc4
Danciu <alexandru.danciu@man.eu>
2019-02-28 15:28:06 +0100

    Added script to check for python dependencies

---
+++ rpi-tensorflow/checkDependencies.py
@@ -0,0 +1,7 @@
+modules = set(["numpy", "os", "six.moves.urllib", "sys", "tarfile", "tensorflow", "zipfile", "cv2", "collections", "io", "matplotlib", "PIL", "utils"])
+
+for module in modules:
+    try:
+        __import__(module)
+    except ImportError:
+        print '{0} not found'.format('module')
