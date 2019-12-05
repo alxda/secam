56e2b2eb0f5518a7d267c1f79c556c576d9e9153
alexandru.danciu@gmail.com <alexandru.danciu@gmail.com>
2019-12-05 16:18:38 +0100

    Test - print path variable

--- rpi-tensorflow/objectDetection.py
+++ rpi-tensorflow/objectDetection.py
@@ -11,8 +11,10 @@ from collections import defaultdict
 from io import StringIO
 from matplotlib import pyplot as plt
 from PIL import Image
-from object_detection.utils import label_map_util
+from utils import label_map_util
 from utils import visualization_utils as vis_util
+
+print(sys.path)
 
 # Define the video stream
 cameraStream = cv2.VideoCapture(0)  # Change only if you have more than one webcams
